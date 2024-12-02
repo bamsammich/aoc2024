5b280308ba751922643c044a8e2e25ee8cb13d6e
Travis Huddleston <travis.huddleston@immuta.com>
2024-12-02 15:43:37 -0500

    cleaner main

--- src/main.rs
+++ src/main.rs
@@ -6,8 +6,6 @@ mod puzzle;
 fn main() {
     let days: Vec<Box<dyn DailyPuzzle>> = vec![day01::Day01::new()];
     for d in days {
-        println!("=== {} ===", d.name());
-        println!("- Test 01: {}", d.test01());
-        println!("- Test 02: {}", d.test02());
+        d.run();
     }
 }
--- src/puzzle.rs
+++ src/puzzle.rs
@@ -2,4 +2,9 @@ pub trait DailyPuzzle {
     fn name(&self) -> String;
     fn test01(&self) -> String;
     fn test02(&self) -> String;
+    fn run(&self) {
+        println!("=== {} ===", self.name());
+        println!("- Test 01: {}", self.test01());
+        println!("- Test 02: {}", self.test02());
+    }
 }
