e813148edd63146d840576ac02a50d5553a9bdc8
Travis Huddleston <travis.huddleston@immuta.com>
2024-12-05 12:14:31 -0500

    day 3 part 2 working

--- src/day03/mod.rs
+++ src/day03/mod.rs
@@ -12,28 +12,16 @@ impl Day03 {
     }
 
     fn part1(&self, input: &str) -> String {
-        let re = Regex::new(r"mul\((?P<left>\d+),(?P<right>\d+)\)").unwrap();
-        let v: i32 = re
-            .captures_iter(input)
-            .map(|cap| {
-                cap.name("left")
-                    .unwrap()
-                    .as_str()
-                    .parse::<i32>()
-                    .expect("valid number")
-                    * cap
-                        .name("right")
-                        .unwrap()
-                        .as_str()
-                        .parse::<i32>()
-                        .expect("valid number")
-            })
-            .sum();
-        v.to_string()
+        add_multiples(input).to_string()
     }
 
     fn part2(&self, input: &str) -> String {
-        unimplemented!()
+        let re_enabled = Regex::new(r"do\(\)(?P<enabled>.*?)don't\(\)").unwrap();
+        re_enabled
+            .captures_iter(format!("do(){input}don't()").as_str())
+            .map(|cap| add_multiples(cap.name("enabled").unwrap().as_str()))
+            .sum::<i32>()
+            .to_string()
     }
 }
 
@@ -51,11 +39,31 @@ impl DailyPuzzle for Day03 {
     }
 }
 
+fn add_multiples(input: &str) -> i32 {
+    let re = Regex::new(r"mul\((?P<left>\d+),(?P<right>\d+)\)").unwrap();
+    re.captures_iter(input)
+        .map(|cap| {
+            cap.name("left")
+                .unwrap()
+                .as_str()
+                .parse::<i32>()
+                .expect("valid number")
+                * cap
+                    .name("right")
+                    .unwrap()
+                    .as_str()
+                    .parse::<i32>()
+                    .expect("valid number")
+        })
+        .sum()
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
 
-    const EXAMPLE: &str = "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))";
+    const EXAMPLE: &str =
+        "xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))";
 
     #[test]
     fn part1() {
@@ -64,10 +72,10 @@ mod tests {
         assert_eq!(d.part1(EXAMPLE), "161");
     }
 
-    // #[test]
-    // fn part2() {
-    //     let d = Day03::new();
+    #[test]
+    fn part2() {
+        let d = Day03::new();
 
-    //     assert_eq!(d.part2(EXAMPLE), "4");
-    // }
+        assert_eq!(d.part2(EXAMPLE), "48");
+    }
 }
